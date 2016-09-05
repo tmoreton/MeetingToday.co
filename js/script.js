b4ed2634de7128bbafe588118cd88c33d6be1ad2
Tim Moreton <tmoreton89@gmail.com>
2016-09-05 15:38:39 -0400

    working video chat

--- js/script.js
+++ js/script.js
@@ -1,22 +1,27 @@
-var room = getParams('name')
-$("#room-name").text("Room " + room.toUpperCase())
+var room = window.location.search.replace("?", "");
 $("#room-url").text(window.location.href)
-$("#modal").click(function(){
-
-})
 
 var webrtc = new SimpleWebRTC({
-    localVideoEl: '',
-    remoteVideosEl: '',
-    autoRequestMedia: false,
+    localVideoEl: 'localVideo',
+    remoteVideosEl: 'remoteVideos',
+    autoRequestMedia: { audio: true, video: true },
     receiveMedia: {
-        offerToReceiveAudio: 0,
-        offerToReceiveVideo: 0
+        offerToReceiveAudio: 1,
+        offerToReceiveVideo: 1
     }
 });
 
-webrtc.joinRoom(room, function(test){
-    webrtc.sendToAll('chat', {message: 'hello bitches', users: webrtc.webrtc.peers.length});
+webrtc.on('readyToCall', function () {
+    webrtc.joinRoom(room, function(){
+        webrtc.sendToAll('chat', {message: 'hello bitches', users: webrtc.webrtc.peers.length});
+    });
+    webrtc.pauseVideo();
+    webrtc.mute();
+});
+
+webrtc.on('createdPeer', function (peer) {
+    var peer = peer;
+    getPeer(peer);
 });
 
 webrtc.connection.on('message', function(data){
@@ -25,95 +30,25 @@ webrtc.connection.on('message', function(data){
     }
 });
 
-var checkPeers = function(){
-    if (webrtc.webrtc.peers.length < 1){
-        $("#waiting").css("display", "block");
-    } else if(webrtc.webrtc.peers.length >= 1){
-        $("#waiting").css("display", "none");
-    }
-}
-
-window.setInterval(checkPeers, 500);
-
 $( "#notes" ).keyup(function() {
   var notes = $( "#notes" ).val()
   webrtc.sendToAll('chat', {message: notes, users: webrtc.webrtc.peers.length});
 });
 
-// webrtc.on('createdPeer', function (peer) {
-//     console.log("someone joined!!", peer.parent.peers.length)
-//     if (peer.parent.peers.length > 0){
-//         $("#waiting").css("display", "none")
-//     }
-// });
-
-// webrtc.on('leftRoom', function (roomName) {
-//     console.log("someone left...", roomName)
-// });
 
 $("#start-video-button").click(function(){
     $("#start-video-button").css("color", "#00b200");
     $("#start-audio-button").css("display", "none");
-    var webrtc = new SimpleWebRTC({
-        localVideoEl: 'localVideo',
-        remoteVideosEl: 'remoteVideos',
-        autoRequestMedia: { audio: true, video: true },
-        receiveMedia: {
-            offerToReceiveAudio: 1,
-            offerToReceiveVideo: 1
-        }
-    });
-
-    
-    webrtc.on('readyToCall', function () {
-        webrtc.joinRoom(room);
-    });
-
-    webrtc.on('createdPeer', function (peer) {
-        var peer = peer;
-        getPeer(peer);
-    });
+    webrtc.resumeVideo();
+    webrtc.unmute();
 })
 
 $("#start-audio-button").click(function(){
     $("#start-audio-button").css("color", "#00b200");
     $("#start-video-button").css("display", "none");
-    var webrtc = new SimpleWebRTC({
-        localVideoEl: 'localVideo',
-        remoteVideosEl: 'remoteVideos',
-        media: {
-            audio: true,
-            video: false
-        },
-        autoRequestMedia: true,
-        receiveMedia: {
-            offerToReceiveAudio: 0,
-            offerToReceiveVideo: 0
-        }
-    });
-
-    webrtc.on('readyToCall', function () {
-        webrtc.joinRoom(room);
-    });
+    webrtc.unmute();
 })
 
-$("#start-file-button").click(function(){
-    var webrtc = new SimpleWebRTC({
-        localVideoEl: '',
-        remoteVideosEl: '',
-        autoRequestMedia: false,
-        receiveMedia: {
-            offerToReceiveAudio: 0,
-            offerToReceiveVideo: 0
-        }
-    });
-
-    webrtc.joinRoom(room);
-    webrtc.on('createdPeer', function (peer) {
-        var peer = peer;
-        getPeer(peer);
-    });
-})
 
 $(".fa-file").click(function(){
     $(".modal").addClass("is-active")
@@ -150,20 +85,19 @@ function getPeer(peer){
     fileinput.type = 'file';
 
     fileinput.addEventListener('change', function() {
-        fileinput.disabled = true;
-
+        // fileinput.disabled = true;
         var file = fileinput.files[0];
         var sender = peer.sendFile(file);
+        $(".modal").removeClass("is-active")
     });
 }
 
-function getParams(name, url) {
-    if (!url) {
-     url = window.location.href;
+var checkPeers = function(){
+    if (webrtc.webrtc.peers.length < 1){
+        $("#waiting").css("display", "block");
+    } else if(webrtc.webrtc.peers.length >= 1){
+        $("#waiting").css("display", "none");
     }
-    var results = new RegExp('[\\?&]' + name + '=([^&#]*)').exec(url);
-    if (!results) { 
-        return undefined;
-    }
-    return results[1] || undefined;
 }
+
+window.setInterval(checkPeers, 500);
